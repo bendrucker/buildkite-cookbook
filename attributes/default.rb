34957127e50a782f5ba92ea3a75f9cc5214fc4cf
Ben Drucker <bvdrucker@gmail.com>
2016-12-07 17:47:41 -0800

    Bump to 3.0 and add default conf

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,9 @@
 default['buildkite']['token'] = ['credentials', 'buildkite', 'token']
-default['buildkite']['version'] = '2.3.2'
+default['buildkite']['version'] = '3.0-beta.16'
 default['buildkite']['architecture'] = '32'
 default['buildkite']['conf_path'] = ::File.join(Chef::Config[:file_cache_path], 'buildkite-agent.cfg')
-default['buildkite']['conf'] = {}
+
+default['buildkite']['conf'] = {
+  'name' => '%hostname-%n',
+  'build-path' => 'builds'
+}
