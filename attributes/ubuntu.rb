b89d4c8f202e35370a7ffc22a215cdb59accde9d
Ben Drucker <bvdrucker@gmail.com>
2016-12-12 08:56:39 -0800

    ubuntu: add ssh_path attr for consumption in other cookbooks

--- attributes/ubuntu.rb
+++ attributes/ubuntu.rb
@@ -1,5 +1,6 @@
 if platform?('ubuntu')
   default['buildkite']['version'] = '2.3.2-1408'
+  default['buildkite']['ssh_path'] = '/var/lib/buildkite-agent/.ssh/'
   default['buildkite']['conf_path'] = '/etc/buildkite-agent/buildkite-agent.cfg'
   default['buildkite']['conf']['build-path'] = '/var/lib/buildkite-agent/builds/'
   default['buildkite']['conf']['bootstrap-script'] = '/usr/share/buildkite-agent/bootstrap.sh'
