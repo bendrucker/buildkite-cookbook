7006cea3e4312df0dd7eb9a38cb3e53f0b436dec
Ben Drucker <bvdrucker@gmail.com>
2016-12-12 08:44:39 -0800

    ubuntu: fix build-path

--- attributes/ubuntu.rb
+++ attributes/ubuntu.rb
@@ -1,7 +1,7 @@
 if platform?('ubuntu')
   default['buildkite']['version'] = '2.3.2-1408'
   default['buildkite']['conf_path'] = '/etc/buildkite-agent/buildkite-agent.cfg'
-  default['buildkite']['conf']['build-path'] = '/etc/buildkite-agent/buildkite-agent.cfg'
+  default['buildkite']['conf']['build-path'] = '/var/lib/buildkite-agent/builds/'
   default['buildkite']['conf']['bootstrap-script'] = '/usr/share/buildkite-agent/bootstrap.sh'
   default['buildkite']['conf']['meta-data']['os'] = 'ubuntu'
 end
