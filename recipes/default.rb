ee1969ae63fe49b8a83d5588f593fe442baa9c99
Ben Drucker <bvdrucker@gmail.com>
2016-12-29 12:20:39 -0800

    Add prefix + bump conf logging to info

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
 include_recipe 'git'
 
 log 'conf' do
-  level :debug
-  message Chef::JSONCompat.to_json_pretty(node['buildkite'])
+  level :info
+  message 'conf: ' + Chef::JSONCompat.to_json_pretty(node['buildkite'])
 end
 
 include_recipe 'buildkite::windows' if platform?('windows')
