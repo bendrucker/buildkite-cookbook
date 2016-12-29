06f8d08256e340a62bd9976a3f3c08d88ed07c79
Ben Drucker <bvdrucker@gmail.com>
2016-12-29 12:01:04 -0800

    Print buildkite conf as JSON in debug logs

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,9 @@
 include_recipe 'git'
+
+log 'conf' do
+  level 'debug'
+  message Chef::JSONCompat.to_json_pretty(node['buildkite'])
+end
+
 include_recipe 'buildkite::windows' if platform?('windows')
 include_recipe 'buildkite::ubuntu' if platform?('ubuntu')
