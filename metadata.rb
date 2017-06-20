34a2e3935dceb2f0397ef4514d156b9d5928d59e
Ben Drucker <bvdrucker@gmail.com>
2017-06-19 17:04:13 -0700

    apt: use >= 6.1

--- metadata.rb
+++ metadata.rb
@@ -16,7 +16,7 @@ supports 'windows'
 
 chef_version '>= 12'
 
-depends 'apt', '~> 5.0.0'
+depends 'apt', '>= 6.1'
 depends 'git', '~> 6.1.0'
 depends 'windows', '~> 3.1.0'
 depends 'winsw', '~> 1.2.2'
